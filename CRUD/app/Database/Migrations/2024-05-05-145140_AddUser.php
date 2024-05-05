f4cb757d0b40bdc479736305adde882ca14b2aed
wamp96 <wamoreno57@gmail.com>
2024-05-05 10:21:57 -0500

    Instalacion y configuracion de Codeigniter/Composer 1 PARTE

---
+++ CRUD/app/Database/Migrations/2024-05-05-145140_AddUser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddUser extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 255,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'unique' => true,
+                'constraint' => '255',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'role' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
