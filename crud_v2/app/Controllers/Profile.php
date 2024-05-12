e8dd291c7cab18c9c245d740730ad5ace7292373
wamp96 <wamoreno57@gmail.com>
2024-05-12 15:22:47 -0500

    Correccion de fallas presentadas en la ejecucion

--- crud_v2/app/Controllers/Profile.php
+++ crud_v2/app/Controllers/Profile.php
@@ -9,8 +9,9 @@
         public function index()
         {
             $ProfileModel = new ProfileModel();
-            $data['profiles'] = $ProfileModel->ordenBy('id','DESC')->findAll();
+            $data['profiles'] = $ProfileModel->orderBy('id','DESC')->findAll();
             return view('profile_view', $data);
+            console.log(view);
         }
 
         public function create()
@@ -27,14 +28,14 @@
                 'name' => $this->request->getVar('name'),
             ];
             $ProfileModel->insert($data);
-            return $this->response->redirect(site_url('/profile-list'));
+            return $this->response->redirect(site_url('/profiles-list'));
         }
 
         public function singleProfile($id = null)
         {
             $ProfileModel = new ProfileModel();
             $data['profile_obj'] = $ProfileModel->where('id',$id)->first();
-            return view('edit_view, $data');
+            return view('edit_view', $data);
         }
 
         public function update()
@@ -47,13 +48,13 @@
                 'name' => $this->request->getVar('name'),
             ];
             $ProfileModel->update($id, $data);
-            return $this->response->redirect(site_url('/profile-list'));
+            return $this->response->redirect(site_url('/profiles-list'));
         }
 
         public function delete($id = null)
         {
             $ProfileModel = new ProfileModel();
             $data['profile'] = $ProfileModel->where('id', $id)->delete($id);
-            return $this->response->redirect(site_url('/profile-list'));
+            return $this->response->redirect(site_url('/profiles-list'));
         }
     }
